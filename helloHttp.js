588123643ba21adff179ba05c686c0f785918b0c
p_wijnhorst@hotmail.com <p_wijnhorst@hotmail.com>
2015-09-14 21:15:42 +0200

    Listen to port 8881 - uncomment http server

--- helloHttp.js
+++ helloHttp.js
@@ -1,19 +1,8 @@
-﻿//var http = require('http');
+﻿var http = require('http');
 
-//var server = http.createServer(function (request, response) {
-//    response.writeHead(200, { "Content-Type": "text/plain" });
-//    response.end("Hello World\n");
-//});
-
-//server.listen(7000);
-
-
-var serialport = require('serialport');
-var SerialPort = serialport.SerialPort;
-
-// list serial ports:
-serialport.list(function (err, ports) {
-    ports.forEach(function (port) {
-        console.log(port.comName);
-    });
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World from Node.js\n");
 });
+
+server.listen(8881);
