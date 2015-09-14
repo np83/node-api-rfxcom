9c0662e326338bba4b69f49d3c8d64a1e5f93fe4
p_wijnhorst@hotmail.com <p_wijnhorst@hotmail.com>
2015-09-14 19:23:17 +0200

    Add hello world samples.

---
+++ helloCons.js
@@ -0,0 +1,2 @@
+console.log('Hello world');
+//# sourceMappingURL=app2.js.map
---
+++ helloHttp.js
@@ -0,0 +1,19 @@
+﻿//var http = require('http');
+
+//var server = http.createServer(function (request, response) {
+//    response.writeHead(200, { "Content-Type": "text/plain" });
+//    response.end("Hello World\n");
+//});
+
+//server.listen(7000);
+
+
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+// list serial ports:
+serialport.list(function (err, ports) {
+    ports.forEach(function (port) {
+        console.log(port.comName);
+    });
+});
