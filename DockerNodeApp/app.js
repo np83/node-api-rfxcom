4be6f7cae68a9afc23367de5be3eba7611ed39c8
root <root@black-pearl.(none)>
2015-09-15 22:13:27 +0200

    current docker image

---
+++ DockerNodeApp/app.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World from Node.js\n");
+});
+
+server.listen(8881);
